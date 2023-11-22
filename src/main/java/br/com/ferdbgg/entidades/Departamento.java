87f1b58fa55a0056e78e2764c4cef2094a7d406f
Fernando Bagagi <fernandosmartphone96@gmail.com>
2023-11-22 08:46:26 -0300

    Cria Departamento

---
+++ src/main/java/br/com/ferdbgg/entidades/Departamento.java
@@ -0,0 +1,23 @@
+package br.com.ferdbgg.entidades;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+public class Departamento implements Serializable {
+
+    private Integer id;
+    private String nome;
+    
+}
