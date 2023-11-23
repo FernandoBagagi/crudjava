3102913d64c01ac14cbc079f9b35822692ca401e
Fernando Bagagi <fernandosmartphone96@gmail.com>
2023-11-23 08:17:19 -0300

    altera anotação do lombok

--- src/main/java/br/com/ferdbgg/entidades/Departamento.java
+++ src/main/java/br/com/ferdbgg/entidades/Departamento.java
@@ -3,18 +3,12 @@ package br.com.ferdbgg.entidades;
 import java.io.Serializable;
 
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
-@EqualsAndHashCode
-@ToString
+@Data
 public class Departamento implements Serializable {
 
     private Integer id;
