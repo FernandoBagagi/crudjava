3dc77c16bce956cfc8e68c13903c08e6e5a764c3
Fernando Bagagi <fernandosmartphone96@gmail.com>
2023-11-23 08:59:08 -0300

    cria DepartamentoDAOJDBC

---
+++ src/main/java/br/com/ferdbgg/model/dao/impl/DepartamentoDAOJDBC.java
@@ -0,0 +1,40 @@
+package br.com.ferdbgg.model.dao.impl;
+
+import java.util.List;
+
+import br.com.ferdbgg.model.dao.EntidadeDAO;
+import br.com.ferdbgg.model.entidades.Departamento;
+
+public class DepartamentoDAOJDBC implements EntidadeDAO<Departamento>{
+
+    @Override
+    public void inserir(Departamento entidade) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'inserir'");
+    }
+
+    @Override
+    public void atualizar(Departamento entidade) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'atualizar'");
+    }
+
+    @Override
+    public void deletarPorID(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deletarPorID'");
+    }
+
+    @Override
+    public Departamento encontrarPorID(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'encontrarPorID'");
+    }
+
+    @Override
+    public List<Departamento> listar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listar'");
+    }
+    
+}
