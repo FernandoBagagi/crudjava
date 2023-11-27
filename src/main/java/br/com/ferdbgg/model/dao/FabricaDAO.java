9f081076c82027b33dbe722b1013d2dacc2f94a2
Fernando Bagagi <fernandosmartphone96@gmail.com>
2023-11-27 19:17:56 -0300

    muda lógica da FabricaDAO

--- src/main/java/br/com/ferdbgg/model/dao/FabricaDAO.java
+++ src/main/java/br/com/ferdbgg/model/dao/FabricaDAO.java
@@ -5,16 +5,24 @@ import java.util.Map;
 
 import br.com.ferdbgg.bancodedados.BD;
 import br.com.ferdbgg.model.dao.impl.DepartamentoDAOJDBC;
+import br.com.ferdbgg.model.dao.impl.VendedorDAOJDBC;
+import br.com.ferdbgg.model.entidades.Departamento;
+import br.com.ferdbgg.model.entidades.Vendedor;
 
 public class FabricaDAO {
     
-    public static final Map<String, EntidadeDAO<? extends Object>> DAOS = new HashMap<>();
+    private static final Map<String, EntidadeDAO<? extends Object>> DAOS = new HashMap<>();
 
     static {
-        FabricaDAO.DAOS.put("Departamento", new DepartamentoDAOJDBC(BD.getConexao()));
+        FabricaDAO.DAOS.put(Departamento.class.getName(), new DepartamentoDAOJDBC(BD.getConexao()));
+        FabricaDAO.DAOS.put(Vendedor.class.getName(), new VendedorDAOJDBC(BD.getConexao()));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends Object> EntidadeDAO<T> getDAO(Class<T> classe) {
+        return (EntidadeDAO<T>)FabricaDAO.DAOS.get(classe.getName());
     }
     
-    private FabricaDAO() {
+    private FabricaDAO() {}
 
-    }
 }
